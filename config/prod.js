cc26222939c6af1e773c80d1f66e1cebb17788fe
Barak Lebowitz <barak.lebowitz@zoominfo.com>
2023-08-18 16:40:06 +0300

    test: mongo connection

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ const username = process.env.MONGO_USERNAME || 'theUser'
 const password = process.env.MONGO_PASSWORD || 'thePass'
 
 module.exports = {
-  dbURL: `mongodb+srv://${username}:${password}@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority`,
+  dbURL: `mongodb+srv://${username}:${password}@cluster0.xf6lg.mongodb.net/?retryWrites=true&w=majority`,
   dbName:'jello_db'
 }
